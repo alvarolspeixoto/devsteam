c9eace21e34469ed0ff808a52343b48f6e1c33c8
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2023-07-14 21:03:46 -0300

    Criado layout de promoções no index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Head from 'next/head'
 
+import styles from '@/styles/index.module.css'
 import Navbar from '@/components/navbar/navbar'
 import Subtitle from '@/components/tipography/subtitle/subtitle'
 import Container from '@/components/container/container'
 import SaleCard from '@/components/cards/saleCard/saleCard'
+
 
 export default function Home() {
   return (
@@ -17,13 +19,15 @@ export default function Home() {
       <div>
         <Navbar />
         <Container>
-        <div>
+        <div className={styles.session}>
           <Subtitle>Promoções</Subtitle>
-          <div>
+          <div className={styles.salecontainer}>
+            <SaleCard />
+            <SaleCard />
             <SaleCard />
           </div>
         </div>  
-        <div>
+        <div className={styles.session}>
           <Subtitle>Outros jogos</Subtitle>
         </div>  
         </Container>
