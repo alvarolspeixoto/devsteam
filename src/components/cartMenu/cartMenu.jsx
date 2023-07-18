83580e6e65ac5b293ec02446cb0e68b69c27dd85
Álvaro <alvaro.peixoto@dcomp.ufs.br>
2023-07-18 20:15:18 -0300

    Criado cartMenu

--- src/components/cartMenu/cartMenu.jsx
+++ src/components/cartMenu/cartMenu.jsx
@@ -1,9 +1,18 @@
 import styles from './cartMenu.module.css'
+import CartOption from '../cartOption/cartOption'
 
 export default function CartMenu(){
     return(
         <div className={styles.menu}>
-            Olá
+            <div className={styles.options}>
+                <CartOption />
+                <CartOption />
+                <CartOption />
+            </div>
+            <div className={styles.priceline}>
+                <h2>Total</h2>
+                <h2 className={styles.price}>R$ 999,90</h2>
+            </div>
         </div>
     )
 }
